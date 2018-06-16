15315e68db6ea4515c6d3cb2a6d6bceece972623
Tiemi Salli <tsalli8@yahoo.com.br>
2018-06-16 20:56:07 -0300

    Este commit serviu para completar o exercicio

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 
-let idade = prompt ("Quantos anos você tem?");
-let renda = prompt ("Qual a sua renda mensal em reais?");
-let valor = prompt ("Qual é o valor que deseja empréstimo?");
-//valor =  number (valor); >1000; <15*renda;
-if (idade <22 || idade >55){
-    alert("Reprovado!")
-    idade=false;
+let idade = prompt ("Quantos anos você tem?")
+let renda = prompt ("Qual a sua renda mensal em reais?")
+let valor = prompt ("Qual é o valor que deseja empréstimo?")
+
+let cliente = {idade,valor,renda}
+
+if (isNaN(idade) || isNaN(renda) || isNaN(valor)){
+        alert("ERRO: VALOR INVÁLIDO");
     }
-if (valor>renda*15){
-    alert("Reprovado")
-    valor=false;
-}    
 
+else if (idade <22 || idade >55 || valor>renda*15 || valor<1000){
+    alert("Reprovado!");
+    cliente=false;
+    }
 
-
-//while(cliente=true){
-//   alert("Aprovado!");
-//    }
-
-
-//else if {
-//alert("Reprovado!");}
+else {
+    alert("Aprovado");
+    cliente=true;
+}
