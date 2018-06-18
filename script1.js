ee689664ca87cf16c1b5d9bb47976f29c106b3a7
Tiemi Salli <tsalli8@yahoo.com.br>
2018-06-18 12:07:09 -0300

    Extra do exercicio 1

--- script1.js
+++ script1.js
@@ -2,6 +2,10 @@
 let idade = prompt ("Quantos anos você tem?")
 let renda = prompt ("Qual a sua renda mensal em reais?")
 let valor = prompt ("Qual é o valor que deseja empréstimo?")
+let parcelas = prompt ("Em quantas parcelas?");
+
+let total = valor*(Math.pow((1+0.08),parcelas));
+let mes = total/parcelas;
 
 let cliente = {idade,valor,renda}
 
@@ -9,12 +13,14 @@ if (isNaN(idade) || isNaN(renda) || isNaN(valor)){
         alert("ERRO: VALOR INVÁLIDO");
     }
 
-else if (idade <22 || idade >55 || valor>renda*15 || valor<1000){
-    alert("Reprovado!");
+else if (idade<22 || idade>55 || valor>renda*15 || valor<1000 || parcelas<3 || parcelas>20){
+    alert("Reprovado");
     cliente=false;
     }
 
 else {
-    alert("Aprovado");
+    alert(`Aprovado! Valor pago mensalmente será: ${mes}`);
     cliente=true;
 }
+
+
